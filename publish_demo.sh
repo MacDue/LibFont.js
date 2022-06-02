eaeb5615af606fab55b0749395a4a08b28ebbe86
MacDue <macdue@dueutil.tech>
2022-06-02 20:22:53 +0100

    Meta: Script to build/publish demo

---
+++ publish_demo.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Needlessly complex script to update GitHub pages
+shopt -s extglob
+read -p "Are you sure? This will delete uncommitted changes... " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+git branch -D demo
+git checkout --orphan demo
+cd glyph_table_window
+npm i
+npm run build
+cd ..
+cp -r glyph_table_window/build ./demo
+mv ./demo/index.html ./demo/glyph_table.html
+cp ./*.html demo
+cp ./*.js demo
+mkdir project_src
+mv !(demo|project_src) ./project_src
+mv demo/* .
+rm -r ./demo
+git add !(project_src)
+git commit -m "Publish demo"
+git push -f --set-upstream origin demo
+git checkout master
+git branch -d demo
+git reset
+git checkout .
+git clean -fd
+rm -rf project_src/
